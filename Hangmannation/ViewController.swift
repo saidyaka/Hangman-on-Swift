5cff955524d515d1bce7be0d5981a70e38859f21
Lord Swag master <saidyaka@saids-macbook-pro.local>
2017-12-21 12:22:03 -0500

    Initial Commit

---
+++ Hangmannation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hangmannation
+//
+//  Created by Said Yaka on 12/21/17.
+//  Copyright © 2017 Said Yaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
